4a845a7927e60cf271eab18efd44b8e0a1ed4858
Daniel Remetan <remdab17@htl-kaindorf.at>
2021-03-24 14:01:57 +0100

    Initial commit

---
+++ desktop/src/at/kaindorf/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package at.kaindorf.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import at.kaindorf.KingOfTerromia;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KingOfTerromia(), config);
+	}
+}
