bf3e037397afbf94c8b82ccbc91566b12cc91557
MrChampion66 <67464742+MrChampion66@users.noreply.github.com>
2021-04-07 16:11:53 +0200

    Option Screen und änderung an Main Menu

--- core/src/at/kaindorf/MenuScreen.java
+++ core/src/at/kaindorf/MenuScreen.java
@@ -1,6 +1,5 @@
 package at.kaindorf;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
@@ -9,11 +8,24 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
+/**
+ * @author: Meister Michael
+ * @date: 24.03.2021
+ * @project: KingOfTerromia
+ */
 public class MenuScreen extends ScreenAdapter {
 
     SpriteBatch batch;
     Texture img;
+    private Stage stage = new Stage();
+    ImageButton start,neuesSpiel,option,beenden;
 
     public MenuScreen() {
         batch = new SpriteBatch();
@@ -29,21 +41,45 @@ public class MenuScreen extends ScreenAdapter {
         font.setColor(Color.RED);
         SpriteBatch batch = new SpriteBatch();
         batch.begin();
-        font.draw(batch, "Press to Start",200,200);
+        stage.draw();
+        stage.act(delta);
         batch.end();
-        if(Gdx.input.isButtonPressed(Input.Buttons.RIGHT)){
-            KingOfTerromia.INSTANCE.setScreen(new GameScreen());
-        }
+    }
+
+    @Override
+    public void show() {
+        start = new ImageButton(new TextureRegionDrawable(new TextureRegion(img)));
+        beenden = new ImageButton(new TextureRegionDrawable(new TextureRegion(img)));
+        start.setPosition(960,700);
+        beenden.setPosition(960,200);
+        stage.addActor(start);
+        stage.addActor(beenden);
+        Gdx.input.setInputProcessor(stage);
+
+        start.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y){
+                KingOfTerromia.INSTANCE.setScreen(new GameScreen());
+            }
+        });
+
+        beenden.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y){
+                System.exit(0);
+            }
+        });
     }
 
     @Override
     public void dispose() {
         batch.dispose();
         img.dispose();
+        stage.dispose();
     }
 
     @Override
     public void hide() {
         this.dispose();
     }
+
+
 }
--- desktop/src/at/kaindorf/desktop/DesktopLauncher.java
+++ desktop/src/at/kaindorf/desktop/DesktopLauncher.java
@@ -1,6 +1,7 @@
 package at.kaindorf.desktop;
 
-import at.kaindorf.beans.AttackCard;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import at.kaindorf.KingOfTerromia;
@@ -9,6 +10,7 @@ import at.kaindorf.KingOfTerromia;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "King Of Terromia";
 		config.width = LwjglApplicationConfiguration.getDesktopDisplayMode().width;
 		config.height = LwjglApplicationConfiguration.getDesktopDisplayMode().height;
 		config.fullscreen = true;
