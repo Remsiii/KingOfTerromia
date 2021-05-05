087d614c9114f2cfad0a21fb4ea88f6edfd2437c
MrChampion66 <67464742+MrChampion66@users.noreply.github.com>
2021-05-05 16:12:49 +0200

    SLider changes Music Volume and Music plays

--- core/src/at/kaindorf/MenuScreen.java
+++ core/src/at/kaindorf/MenuScreen.java
@@ -3,6 +3,8 @@ package at.kaindorf;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -25,14 +27,18 @@ public class MenuScreen extends ScreenAdapter {
     SpriteBatch batch;
     Texture img,play,quit,opt;
     private Stage stage = new Stage();
-    ImageButton start,neuesSpiel,option,beenden;
+    ImageButton start,option,beenden;
+    public static Music music = Gdx.audio.newMusic(Gdx.files.internal("MainMenuOST.mp3"));
 
     public MenuScreen() {
+        music.play();
+        music.setLooping(true);
         batch = new SpriteBatch();
         img = new Texture("badlogic.jpg");
         play = new Texture("play.png");
         quit = new Texture("quit.png");
         opt = new Texture("opt.png");
+
     }
 
     @Override
@@ -54,6 +60,7 @@ public class MenuScreen extends ScreenAdapter {
         start = new ImageButton(new TextureRegionDrawable(new TextureRegion(play)));
         beenden = new ImageButton(new TextureRegionDrawable(new TextureRegion(quit)));
         option = new ImageButton(new TextureRegionDrawable(new TextureRegion(opt)));
+
         start.setPosition(800,900);
         start.setSize(405,108);
         option.setPosition(800,700);
--- core/src/at/kaindorf/OptionScreen.java
+++ core/src/at/kaindorf/OptionScreen.java
@@ -3,8 +3,10 @@ package at.kaindorf;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -12,10 +14,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
@@ -27,27 +26,32 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 public class OptionScreen extends ScreenAdapter {
     SpriteBatch batch;
     SelectBox<String> screenMode;
-    SelectBox<String> resolution;
     Skin skin;
     private Stage stage = new Stage();
     Dialog dialog;
+    Slider musicVol;
+    BitmapFont font = new BitmapFont();
+    TextField volume;
+    Texture mute,noMute;
+    ImageButton muteBt;
+    boolean isMute = false;
 
     public OptionScreen() {
         skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
         batch = new SpriteBatch();
         screenMode = new SelectBox<String>(skin);
-        resolution = new SelectBox<String>(skin);
         dialog = new Dialog("Setting",skin);
-
+        musicVol = new Slider(0,1,0.01f,false,skin);
+        volume = new TextField("",skin);
+        mute = new Texture("mute.png");
     }
 
     @Override
     public void render(float delta) {
         BitmapFont font = new BitmapFont();
-
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        font.setColor(Color.RED);
+        font.setColor(Color.BLACK);
         SpriteBatch batch = new SpriteBatch();
         batch.begin();
         stage.draw();
@@ -57,21 +61,25 @@ public class OptionScreen extends ScreenAdapter {
 
     @Override
     public void show() {
-        dialog.setSize(Gdx.graphics.getWidth()/2+100,Gdx.graphics.getHeight()/2+100);
-        dialog.setPosition(Gdx.graphics.getWidth()/2 - dialog.getWidth()/2,Gdx.graphics.getHeight()/2 - dialog.getHeight()/2);
-        screenMode.setItems("Vollbild","Fenstermodus");
-        resolution.setItems(Gdx.graphics.getWidth()+"x"+Gdx.graphics.getHeight(),
-                String.format("%.0fx%.0f",Gdx.graphics.getWidth()*0.8333333333333333,Gdx.graphics.getHeight()*0.8333333333333333),
-                String.format("%.0fx%.0f",(Gdx.graphics.getWidth()*0.8333333333333333)*0.8,(Gdx.graphics.getHeight()*0.8333333333333333)*0.8));
+        //dialog.setSize(Gdx.graphics.getWidth()/2+100,Gdx.graphics.getHeight()/2+100);
+        //dialog.setPosition(Gdx.graphics.getWidth()/2 - dialog.getWidth()/2,Gdx.graphics.getHeight()/2 - dialog.getHeight()/2);
+        muteBt = new ImageButton(new TextureRegionDrawable(new TextureRegion(mute)));
 
-        dialog.getContentTable().defaults().pad(10);
-        dialog.getContentTable().add(screenMode);
-        dialog.getContentTable().add(resolution);
-
+        screenMode.setItems("Fullscreen","Windowmode");
+        screenMode.getStyle().listStyle.font.getData().scale(0.5f);
+        screenMode.setSize(320,60);
+        screenMode.setPosition(Gdx.graphics.getWidth()/2,Gdx.graphics.getHeight()/2);
+        musicVol.setPosition(Gdx.graphics.getWidth()/2.5f,Gdx.graphics.getHeight()/2);
+        volume.setPosition(Gdx.graphics.getWidth()/3.2f,Gdx.graphics.getHeight()/2);
+        musicVol.setValue(1);
+        //dialog.getContentTable().defaults().pad(10);
+        //dialog.getContentTable().add(screenMode);
+        volume.setDisabled(true);
+        volume.setText(String.format("%.0f%%",musicVol.getValue()*100));
         screenMode.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                if(screenMode.getSelected().equals("Vollbild")){
+                if(screenMode.getSelected().equals("Fullscreen")){
                     Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
                 }else{
                     Gdx.graphics.setWindowedMode(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
@@ -79,24 +87,32 @@ public class OptionScreen extends ScreenAdapter {
             }
         });
 
-        resolution.addListener(new ChangeListener() {
+        musicVol.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                String[] res = resolution.getSelected().split("x");
-                System.out.println(String.format("%s %s",res[0],res[1]));
-                if(screenMode.getSelected().equals("Fenstermodus")){
-                    Gdx.graphics.setWindowedMode(Integer.parseInt(res[0]),Integer.parseInt(res[1]));
-                    stage.getViewport().update(Integer.parseInt(res[0]),Integer.parseInt(res[1]), true);
-                }else{
+                if(musicVol.isDragging()) {
+                    MenuScreen.music.setVolume(musicVol.getValue());
+                    volume.setText(String.format("%.0f%%",musicVol.getValue()*100));
+                }
 
-                    stage.getViewport().update(Integer.parseInt(res[0]),Integer.parseInt(res[1]), true);
+            }
+        });
+
+        muteBt.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y){
+                isMute = !isMute;
+                if(isMute){
+                    muteBt.setChecked(false);
+                }else{
+                    muteBt.setChecked(true);
                 }
             }
         });
 
-
-
-        stage.addActor(dialog);
+        stage.addActor(volume);
+        stage.addActor(screenMode);
+        stage.addActor(musicVol);
+        stage.addActor(muteBt);
         Gdx.input.setInputProcessor(stage);
     }
 
